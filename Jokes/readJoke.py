252037ced063bad82840b80897c82f4f85bad36f
mathiassmichno <mmichn13@student.aau.dk>
2015-02-17 14:24:28 +0100

    lollll

--- Jokes/readJoke.py
+++ Jokes/readJoke.py
@@ -3,10 +3,11 @@ import random
 import time
 from subprocess import call    
 
-data = "lyrics"
+data = "jokes2"
 num_lines = sum(1 for line in open(data))
 f = open(data, 'r')
 jokes = []
+endLines = ["Top kek", "LOL", "Kappa", "OMG WTF", "ha ha ha", "That's what she said"]
 i = 0 
 
 f.seek(0, 2)
@@ -15,13 +16,22 @@ f.seek(0, 0)
 
 while f.tell() != eof:
 	jokes.append(f.readline() + " ; " + f.readline()) 
-	i += 1
-
-rand = random.randint(0, i) 
-call(["Say", jokes[rand].split(" ; ", 2)[0]]) 
-time.sleep(1)
-call(["Say", jokes[rand].split(" ; ", 2)[1]])  
 	
-	
- 
 
+shuffledJokes = random.shuffle(jokes)
+
+while i < len(jokes):
+	say = jokes[i].split(" ; ", 2)
+	print(say[0])  
+	call(["Say", say[0]]) 
+	time.sleep(1)
+	print(say[1]) 
+	call(["Say", say[1]])
+	wait = random.randint(2,4)
+	time.sleep(wait) 
+	call(["Say", "-v", "Kate", random.choice(endLines)])
+	wait = random.randint(5,300)
+	print(wait)
+	time.sleep(wait) 
+	i += 1   
+	           
