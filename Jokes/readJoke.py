aaa5bd52642e107cc8f06a9e335b98b7023d38fb
mathiassmichno <mmichn13@student.aau.dk>
2015-02-23 09:58:14 +0100

    all stuff

--- Jokes/readJoke.py
+++ Jokes/readJoke.py
@@ -7,7 +7,7 @@ data = "jokes2"
 num_lines = sum(1 for line in open(data))
 f = open(data, 'r')
 jokes = []
-endLines = ["Top kek", "LOL", "Kappa", "OMG WTF", "ha ha ha", "That's what she said"]
+endLines = ["Top kek", "LOL", "Kappa", "what the fuck", "ha ha ha", "Top kek", "Top kek"]
 i = 0 
 
 f.seek(0, 2)
@@ -23,11 +23,14 @@ shuffledJokes = random.shuffle(jokes)
 while i < len(jokes):
 	say = jokes[i].split(" ; ", 2)
 	print(say[0])  
-	call(["Say", say[0]]) 
-	time.sleep(1)
+	call(["Say", say[0]])
+	if say[0].strip().endswith("?"): 
+		time.sleep(1)
+	else:
+		time.sleep(0.25)
 	print(say[1]) 
 	call(["Say", say[1]])
-	wait = random.randint(2,4)
+	wait = random.randint(2,3)
 	time.sleep(wait) 
 	call(["Say", "-v", "Kate", random.choice(endLines)])
 	wait = random.randint(5,300)
